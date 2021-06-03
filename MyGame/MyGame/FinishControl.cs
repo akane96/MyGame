f2d6af6953ed160f07fd782bc521ebb1bdca9ea4
smuhu <smuhut21@mail.ru>
2021-06-03 14:47:31 +0500

    Fixed finish control

--- MyGame/MyGame/FinishControl.cs
+++ MyGame/MyGame/FinishControl.cs
@@ -26,6 +26,9 @@ namespace MyGame
             _game.CreateMonsters();
             _game.CreateProjectile();
             _game.ChangeState(GameState.Battle);
+            _game.Player.Projectiles.Clear();
+            _game.Player.ProjectilesInAction.Clear();
+            _game.Score = 0;
         }
 
         public void Configure(Game game)
